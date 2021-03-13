cc815de678e24e01ae5c80fd12488db9833d05f4
Spencer Curtis <spencercurtis@Spencers-MacBook-Air.local>
2021-03-13 11:45:32 -0700

    controllers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,58 @@
 class ProductsController < ApplicationController
+  before_action: set_department
+  before_action :set_product, only: [ :show, :edit, :update, :destroy ]
+
   def index
+    @products = @departments.products
+    render component: 'Products', props: { department: @department, products: @products }
   end
 
   def show
+    render component: 'Product', props: { department: @department, product: @product }
   end
 
   def new
+    @product = @department.products.new(product_params)
+    render component: 'ProductNew', props: { department: @department, product: @product }
+  end
+
+  def create
+    @product = @department.products.new(product_params)
+    if @product.save
+      redirect_to department_products_path(@department)
+    else
+      render component: 'ProductNew', props { department: @department, product: @product}
+    end
   end
 
   def edit
+    render component: 'ProductEdit', props: {department: @department, products: @product}
   end
+
+  def update
+    if @product.update(product_params)
+      redirect_to department_products_path(@department)
+    else
+      render component: 'ProductEdit', props: { department: @department, product: @product}
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to department_products_path(@department)
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :description, :price, :stock)
+    end
+
+    def set_department
+      @department = Department.find(params[:department_id])
+    end
+
+    def set_product
+      @product = @department.products.find(params[:id])
+    end
+
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,56 @@
 class ReviewsController < ApplicationController
+  before_action: set_product
+  before_action: set_review
   def index
+    @reviews = @products.reviews
+    render component: 'Reviews', props: { product: @product, reviews: @reviews }
   end
 
   def show
+    render component: 'Review', props: { product: @product, review: @review }
   end
 
   def new
+    @review = @product.reviews.new(review_params)
+    render component: 'ReviewNew', props: { product: @product, review: @review }
+  end
+
+  def create
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      redirect_to product_reviews_path(@product)
+    else
+      render component: 'ReviewNew', props { product: @product, review: @review}
+    end
   end
 
   def edit
+    render component: 'ReviewEdit', props: {product: @product, reviews: @reviews}
   end
+
+  def update
+    if @review.update(review_params)
+      redirect_to product_reviews_path(@product)
+    else
+      render component: 'ReviewEdit', props: { product: @product, review: @review }
+    end
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to product_reviews_path(@product)
+  end
+
+  private
+    def review_params
+      params.require(:product).permit(:date, :rating, :subject, :body)
+    end
+
+    def set_product
+      @product = Product.find(params[:product_id])
+    end
+
+    def set_song
+      @review = @product.reviews.find(params[:id])
+    end
 end
